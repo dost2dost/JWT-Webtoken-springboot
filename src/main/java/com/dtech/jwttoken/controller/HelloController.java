680c0687d140d3c6db5643140a5c88065813e5f2
dost2dost <dost2dost@gmail.com>
2020-09-14 17:23:57 +0500

    First commit for jwt token

---
+++ src/main/java/com/dtech/jwttoken/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.dtech.jwttoken.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String firstPage() {
+		return "hello world !";
+	}
+
+}
